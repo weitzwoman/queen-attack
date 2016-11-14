a49926308aa71aa813cd9d263cf77fc61d8731a0
Luke Philips and Tracie Weitzman <luke@>
2016-11-14 15:17:00 -0800

    passes diagonal test

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,10 +1,21 @@
 class Array
   define_method(:queen_attack?) do |position|
+    attack = false
     targetRow = position[0]
     targetColumn = position[1]
     queenRow = self[0]
     queenColumn = self[1]
 
-    queenRow.eql?(targetRow)
+    changeRow = (targetRow - queenRow).abs()
+    changeColumn = (targetColumn - queenColumn).abs()
+
+    if queenRow.eql?(targetRow)
+      attack = true
+    elsif queenColumn.eql?(targetColumn)
+      attack = true
+    elsif changeRow.eql?(changeColumn)
+      attack = true
+    end
+    attack
   end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -6,4 +6,20 @@ describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally in line with each other') do
     expect([1,1].queen_attack?([2,3])).to(eq(false))
   end
+  it('is false if the coordinates are not vertically in line with each other') do
+    expect([1,1].queen_attack?([2,3])).to(eq(false))
+  end
+  it('is false if the coordinates are not diagonally in line with each other') do
+    expect([1,1].queen_attack?([2,3])).to(eq(false))
+  end
+
+  it('is true if the X coordinates are equal') do
+    expect([1,1].queen_attack?([1,3])).to(eq(true))
+  end
+  it('is true if the Y coordinates are equal') do
+    expect([1,1].queen_attack?([3,1])).to(eq(true))
+  end
+  it('is true if the absolute change in X and Y from queenX to TargetX, and queenY to TargetY are equal') do
+    expect([1,1].queen_attack?([3,3])).to(eq(true))
+  end
 end
